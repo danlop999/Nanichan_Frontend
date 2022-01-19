a1d530b69665d41255a3c2593bbea8057b9d7b3d
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-01-19 09:20:16 +0900

    [Update] Home.php -> index.php indexじゃないとルートファイルとして読み込まないため
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- Nanichan_Frontend_Project/headers/header-main.php
+++ Nanichan_Frontend_Project/headers/header-main.php
@@ -12,11 +12,11 @@
         <!--サイト名部分-->
 		<div class="headerBox">
 						
-			<a href="Home.php"><img src="images/nanichan_icon4.png" class="siteName" alt="なにちゃんのアイコン" width="100ex" height="70ex" style="padding-left: 1%;"></a>
+			<a href="index.php"><img src="images/nanichan_icon4.png" class="siteName" alt="なにちゃんのアイコン" width="100ex" height="70ex" style="padding-left: 1%;"></a>
 			<a style="font-size:4em;">なにちゃん</a>
 			
 			<!--左３つのボタン部分-->
-			<a href="HOME.php" class="buttonIcon"><img src="images/HomeButton.png" alt="HOMEボタン" width="140" height="50" style="padding-left: 30%;"></a>
+			<a href="index.php" class="buttonIcon"><img src="images/HomeButton.png" alt="HOMEボタン" width="140" height="50" style="padding-left: 30%;"></a>
 			<a href="./ThreadList.php?categoryid=64&categoryname=FAQ" class="buttonIcon"><img src="images/FaqButton.png" alt="FAQボタン" width="140" height="50" style="text"></a>
 			<a href="UserGuide.php" class="buttonIcon"><img src="images/UserguideButton.png" alt="使い方" width="140" height="50"></a>
 		</div>
